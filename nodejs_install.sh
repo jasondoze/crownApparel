2c6cbd1d5427b88a82a82edeb6cb44575e55b9e0
Jason Doze <dozejason@gmail.com>
2023-02-27 15:49:27 -0600

    Remove crownapp.service from rsync exclude in deploy script.
    Uncomment service file script in nodejs_install.sh.
    
    Add correct npm installs to nodejs_install.sh.
    
    Add corrected path to service file after crown-apparel directory delete.
    
    Signed-off-by: Jason Doze <dozejason@gmail.com>

--- nodejs_install.sh
+++ nodejs_install.sh
@@ -13,14 +13,15 @@ else
   curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - 
 fi
 
-# Install NodeJS and NPM
+# Install NodeJS, NPM, and update browserslist-db@latest
 if ( which node ) 
 then
   echo -e "\n==== NodeJS installed ====\n"
 else 
-  echo -e "\n==== Installing NodeJS && NPM ====\n"
+  echo -e "\n==== Installing NodeJS && NPM && Update Browserlist ====\n"
   sudo apt install -y nodejs  
   sudo apt install -y npm
+  sudo npx update-browserslist-db@latest
 fi
 
 # Install NPM and its dependencies
@@ -29,8 +30,7 @@ then
   echo -e "\n==== Node_modules installed ====\n"
 else 
   echo -e "\n==== Installing node_modules ====\n"
-  sudo npm install -g npm@latest
-  sudo npm install react-scripts
+  npm install 
 fi
 
 # Run NPM build
@@ -39,15 +39,9 @@ then
   echo -e "\n==== NPM build complete ====\n"
 else 
   echo -e "\n==== Running NPM build ====\n"
-  sudo npm i update-browserslist-db
-  sudo npm run build 
-  
+ npm run build 
 fi
 
-echo -e "\n==== Run react app ====\n"
-sudo npm start
-
-<<pause
 # Copy service file and reload daemon
 if [ -f /lib/systemd/system/crownapp.service ] 
 then
@@ -57,7 +51,6 @@ else
   sudo cp crownapp.service /lib/systemd/system/ && sudo systemctl daemon-reload
 fi
 
-
 # Restart the crownapp service
 if ( systemctl is-active crownapp.service ) 
 then
@@ -66,7 +59,7 @@ else
   echo -e "\n==== Starting crownapp ====\n"
   sudo systemctl restart crownapp.service
 fi
-pause
+
 
 echo -e "\n==== Install complete ====\n"
 
